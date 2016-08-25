4b4fc1b5378e170b0e6ab2267d61a32daaf6f99c
Anton Ukhanev <xedin.unknown@gmail.com>
2016-08-25 18:04:22 +0200

    Test class enhanced

--- src/Test/AbstractTest.php
+++ src/Test/AbstractTest.php
@@ -2,6 +2,11 @@
 
 namespace Dhii\SimpleTest\Test;
 
+/**
+ * Common functionality for tests.
+ *
+ * @since [*next-version*]
+ */
 abstract class AbstractTest implements TestInterface
 {
     protected $caseName;
@@ -10,51 +15,89 @@ abstract class AbstractTest implements TestInterface
     protected $key;
     protected $message;
     protected $assertionCount;
-    
+    protected $suiteCode;
+    protected $runnerCode;
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function getCaseName()
     {
         return $this->caseName;
     }
-    
+
+    /**
+     * @param string $name Name of the case, to which this test belongs.
+     * @return AbstractTest This instance.
+     */
     protected function _setCaseName($name)
     {
         $this->caseName = $name;
         return $this;
     }
 
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function getMethodName()
     {
         return $this->methodName;
     }
-    
+
+    /**
+     * @param string $name Name of the method, which this case represents.
+     * @return AbstractTest This instance.
+     */
     protected function _setMethodName($name)
     {
         $this->methodName = $name;
         return $this;
     }
 
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function getStatus()
     {
         return $this->status;
     }
-    
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function setStatus($status)
     {
         $this->status = $status;
         return $this;
     }
-    
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function getKey()
     {
         return $this->key;
     }
-    
+
+    /**
+     * @param string $key Name of the method, which this case represents.
+     * @return AbstractTest This instance.
+     */
     protected function _setKey($key)
     {
         $this->key = $key;
         return $this;
     }
-    
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function getStatusCodes()
     {
         return array(
@@ -63,30 +106,91 @@ abstract class AbstractTest implements TestInterface
             self::SUCCESS
         );
     }
-    
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function setMessage($message)
     {
         $this->message = $message;
         return $this;
     }
-    
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function getMessage()
     {
         return $this->message;
     }
-    
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function isSuccessful()
     {
         return $this->getStatus() === self::SUCCESS;
     }
-    
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
     public function setAssertionCount($assertionCount)
     {
         $this->assertionCount = intval($assertionCount);
         return $this;
     }
-    
-    public function getAssertionCount() {
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
+    public function getAssertionCount()
+    {
         return $this->assertionCount;
+    }
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
+    public function getSuiteCode()
+    {
+        return $this->suiteCode;
+    }
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
+    public function setSuiteCode($code)
+    {
+        $this->suiteCode = $code;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
+    public function getRunnerCode()
+    {
+        return $this->runnerCode;
+    }
+
+    /**
+     * @inheritdoc
+     * @since [*next-version*]
+     */
+    public function setRunnerCode($code)
+    {
+        $this->runnerCode = $code;
+
+        return $this;
     }
 }
