f0f7e2461d685e441f59f6808493533c710c14b0
Anton Ukhanev <xedin.unknown@gmail.com>
2016-08-26 09:43:30 +0200

    Added docs to `DefaultRunner`

--- src/DefaultRunner.php
+++ src/DefaultRunner.php
@@ -2,9 +2,16 @@
 
 namespace Dhii\SimpleTest;
 
+/**
+ * A default runner implementation.
+ *
+ * @since [*next-version*]
+ */
 class DefaultRunner extends AbstractRunner
 {
     /**
+     * @inheritdoc
+     * @since [*next-version*]
      */
     public function getCode()
     {
