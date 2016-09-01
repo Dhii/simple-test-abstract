35977e26f44d7d31ffc408a219ca4d60b909d3f7
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-01 19:39:48 +0200

    Code standards fixed

--- src/Collection/MutableCollectionInterface.php
+++ src/Collection/MutableCollectionInterface.php
@@ -13,7 +13,9 @@ interface MutableCollectionInterface
      * Adds an item to this collection.
      *
      * @param mixed $item An item to add to the collection.
+     *
      * @since [*next-version*]
+     *
      * @return bool True if item has been added; false otherwise.
      */
     public function addItem($item);
@@ -22,16 +24,20 @@ interface MutableCollectionInterface
      * Adds multiple items to this collection.
      *
      * @param mixed[]|\Traversable $items A list of items to add.
+     *
      * @since [*next-version*]
+     *
      * @return bool True if items have been added; false otherwise.
      */
     public function addItems($items);
 
     /**
-     * Removes an item from the collection
+     * Removes an item from the collection.
      *
      * @param mixed $item The item to remove from the collection.
+     *
      * @since [*next-version*]
+     *
      * @return bool True if item has been removed; false otherwise.
      */
     public function removeitem($item);
@@ -40,8 +46,10 @@ interface MutableCollectionInterface
      * Removes an item with the specified key from the collection.
      *
      * @param string|int $key The key of an item to remove.
+     *
      * @since [*next-version*]
+     *
      * @return bool True if item has been removed; false otherwise.
      */
-    public function removeItemByKey($item);
+    public function removeItemByKey($key);
 }
