7d2028c207a606eb118c40707b1d590fe8cb55b6
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-02 11:48:18 +0200

    `CollectionInterface#getItems()` is not more generic
    Docs added.

--- src/Collection/CollectionInterface.php
+++ src/Collection/CollectionInterface.php
@@ -12,9 +12,12 @@ interface CollectionInterface
     /**
      * Retrieve the items of the collection.
      *
+     * This method may return an array. However, this is not a reliable way to convert a collection
+     * to an array. For that purpose, use {@see iterator_to_array()}.
+     *
      * @since [*next-version*]
      *
-     * @return object[] The array of items, by original key.
+     * @return mixed[]|\Traversable The list of items, by original key.
      */
     public function getItems();
 }
