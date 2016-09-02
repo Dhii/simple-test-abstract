43e4bb8bfb7470fdc503ad4e16d2ea57108f17f7
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-02 12:27:03 +0200

    Added `Collection\AppendInterface`

---
+++ src/Collection/AppendIterator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dhii\SimpleTest\Collection;
+
+/**
+ * A standards-compliant implementation of {@see \AppendIterator} that does
+ * not promise to be writable.
+ *
+ * @since [*next-version*]
+ */
+class AppendIterator extends \AppendIterator implements SequenceIteratorIteratorInterface
+{
+}
